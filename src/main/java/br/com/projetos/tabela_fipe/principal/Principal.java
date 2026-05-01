79def771330e98e09a0efc45fbd2bc40f2751c9a
TheV1k <victormoreiramos@gmail.com>
2026-05-01 20:01:07 -0300

    Tratamento  da NullPointerException na inserção do cód marca

--- src/main/java/br/com/projetos/tabela_fipe/principal/Principal.java
+++ src/main/java/br/com/projetos/tabela_fipe/principal/Principal.java
@@ -16,21 +16,29 @@ public class Principal {
     private ConverteDados conversor = new ConverteDados();
     private final String ENDERECO = "https://parallelum.com.br/fipe/api/v1/";
 
-    public void exibeMenu (){
+    public void exibeMenu() {
+
         System.out.println("Digite o tipo de veiculo (Carros/Motos/Caminhões)");
         var tipoVeiculo = sc.nextLine().toLowerCase().trim();
-        var json = consumo.obterDados(ENDERECO +tipoVeiculo +"/marcas/");
-       List<DadosVeiculo> veiculos = conversor.obterLista(json, DadosVeiculo.class);
-       veiculos.forEach(v -> System.out.printf("Cód: %s | Marca: %s\n", v.codigo(),v.marca()));
+        var json = consumo.obterDados(ENDERECO + tipoVeiculo + "/marcas/");
+        List<DadosVeiculo> veiculos = conversor.obterLista(json, DadosVeiculo.class);
+        veiculos.forEach(v -> System.out.printf("Cód: %s | Marca: %s\n", v.codigo(), v.marca()));
 
-        System.out.println("Digite o código da marca");
-        var codMarca = sc.nextLine();
-        json = consumo.obterDados(ENDERECO +tipoVeiculo +"/marcas/" + codMarca + "/modelos/" );
-        var resposta = conversor.obterDados(json, Modelos.class);
-        resposta.modelos().forEach(m ->
-                System.out.printf("Cód: %s | Marca: %s\n", m.cod(), m.descricao()));
-            
-        }
+        while (true)
+            try {
+                System.out.println("Digite o código da marca");
+                var codMarca = sc.nextLine();
+                json = consumo.obterDados(ENDERECO + tipoVeiculo + "/marcas/" + codMarca + "/modelos/");
+                var resposta = conversor.obterDados(json, Modelos.class);
+
+                resposta.modelos().forEach(m ->
+                        System.out.printf("Cód: %s | Marca: %s\n", m.cod(), m.descricao()));
+            } catch (NullPointerException e) {
+
+                System.out.println("Código inválido. Digite um código válido.");
+
+            }
 
     }
+}
 
